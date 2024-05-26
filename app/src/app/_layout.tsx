b0365863e23169d324fae5b3242f90f0cd1097d8
Mubarak Yahaya <ymubarak247@gmail.com>
2024-05-26 23:14:52 +0100

    FT-17 updated auth screens headers

--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -5,7 +5,14 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" />
       <Stack.Screen name="signup" options={{
-        headerShown:false
+        headerShown:true,
+        headerTitle: "",
+        headerBackTitleVisible: false,
+      }} />
+      <Stack.Screen name="signin" options={{
+        headerShown:true,
+        headerTitle: "",
+        headerBackTitleVisible: false,
       }} />
     </Stack>
   );
