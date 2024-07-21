80ea31a36d674faa6f571baccdf756845b54640c
Mubarak Yahaya <ymubarak247@gmail.com>
2024-07-22 00:36:36 +0100

    added a refresh token route

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ const connectDB = require("./db/connect")
 const express = require("express");
 const app = express()
 require("dotenv").config()
+//Routes
 const authRouter = require("./routes/auth");
 const expenseRouter = require("./routes/expense")
+const tokenRouter = require("./routes/token")
+
+// Middlewares
 const authMiddleware = require("./middleware/auth")
 
 
@@ -19,6 +23,7 @@ app.use(express.json())
 // app.use(express.urlencoded({extended: false}));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/expense", authMiddleware, expenseRouter);
+app.use("/api/v1/token", authMiddleware, tokenRouter)
 app.use(errorHandler)
 
 const runDb = async () => {
